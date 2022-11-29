a9c41b8247c590cb73ade310ed51e947ad82b546
cumedang <103031323+cumedang@users.noreply.github.com>
2022-11-29 12:37:10 +0900

    muport

---
+++ scroll.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){       
+    var scroll_pos = 0;
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+        console.log(scroll_pos);
+        if(scroll_pos>2000){
+            $(".home").css('color', '#a8a9b4');
+            $(".main-home").css('color', '#a8a9b4');
+            $(".about").css('color', '#a8a9b4');
+            $(".main-about").css('color', '#a8a9b4');
+            $(".re").css('color', '#a8a9b4');
+            $(".main-re").css('color', '#a8a9b4');
+            $(".port").css('color', '#a8a9b4');
+            $(".main-port").css('color', '#a8a9b4');
+            $(".sk").css('color', '#2ECCFA');
+            $(".main-skill").css('color', '#2ECCFA');
+        }else if(scroll_pos>1406){
+            $(".home").css('color', '#a8a9b4');
+            $(".main-home").css('color', '#a8a9b4');
+            $(".about").css('color', '#a8a9b4');
+            $(".main-about").css('color', '#a8a9b4');
+            $(".re").css('color', '#a8a9b4');
+            $(".main-re").css('color', '#a8a9b4');
+            $(".port").css('color', '#2ECCFA');
+            $(".main-port").css('color', '#2ECCFA');
+            $(".sk").css('color', '#a8a9b4');
+            $(".main-skill").css('color', '#a8a9b4');
+        }else if(scroll_pos >940){
+            $(".home").css('color', '#a8a9b4');
+            $(".main-home").css('color', '#a8a9b4');
+            $(".about").css('color', '#a8a9b4');
+            $(".main-about").css('color', '#a8a9b4');
+            $(".re").css('color', '#2ECCFA');
+            $(".main-re").css('color', '#2ECCFA');
+            $(".port").css('color', '#a8a9b4');
+            $(".main-port").css('color', '#a8a9b4');
+            $(".sk").css('color', '#a8a9b4');
+            $(".main-skill").css('color', '#a8a9b4');
+        }else if(scroll_pos > 384){
+            $(".home").css('color', '#a8a9b4');
+            $(".main-home").css('color', '#a8a9b4');
+            $(".about").css('color', '#2ECCFA');
+            $(".main-about").css('color', '#2ECCFA');
+            $(".re").css('color', '#a8a9b4');
+            $(".main-re").css('color', '#a8a9b4');
+            $(".port").css('color', '#a8a9b4');
+            $(".main-port").css('color', '#a8a9b4');
+            $(".sk").css('color', '#a8a9b4');
+            $(".main-skill").css('color', '#a8a9b4');
+        }else{
+            $(".home").css('color', '#2ECCFA');
+            $(".main-home").css('color', '#2ECCFA');
+            $(".about").css('color', '#a8a9b4');
+            $(".main-about").css('color', '#a8a9b4');
+            $(".re").css('color', '#a8a9b4');
+            $(".main-re").css('color', '#a8a9b4');
+            $(".port").css('color', '#a8a9b4');
+            $(".main-port").css('color', '#a8a9b4');
+            $(".sk").css('color', '#a8a9b4');
+            $(".main-skill").css('color', '#a8a9b4');
+        }
+
+    });
+});
+const options = {
+    root: null, // viewport
+    rootMargin: "0px",
+    threshold: .5,  // 50%가 viewport에 들어와 있어야 callback 실행
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+    console.log(entry.isIntersecting);
+    if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+    } else {
+        entry.target.classList.remove("active");
+    }
+    });
+}, options);
+
+const titleList = document.querySelectorAll('.aboutme');
+titleList.forEach(el => observer.observe(el));
